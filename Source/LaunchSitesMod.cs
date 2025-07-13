d78a2534a6d1fb73897b363fc654ebed05b313fa
ritefall <cassandralilypeters@gmail.com>
2025-07-13 19:55:38 +1000

    Initial setup

---
+++ Source/LaunchSitesMod.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace LaunchSitesExpire
+{
+    [StaticConstructorOnStartup]
+    public class LaunchSitesMod
+    {
+        static LaunchSitesMod()
+        {
+            
+        }
+    }
+}
